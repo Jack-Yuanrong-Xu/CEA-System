56a56477284506869f32d58d7decd740e16d73db
Jack-Yuanrong-Xu <jackyuanrongxu04@gmail.com>
2026-04-06 21:40:44 -0700

    influxDB addition, and grafana installed

--- CEA-System/sensors/SHT31.py
+++ CEA-System/sensors/SHT31.py
@@ -1,11 +1,15 @@
 import board
 import busio
 import adafruit_sht31d
+import time
 
 # Initialize I2C and sensor
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_sht31d.SHT31D(i2c)
+sensor = adafruit_sht31d.SHT31D(i2c, address = 0x44)
 
 # Read and print values
-print(f"Temperature: {sensor.temperature:.1f} °C")
-print(f"Humidity: {sensor.relative_humidity:.1f} %")
+while True:
+    print(f"Temperature: {sensor.temperature:.1f} °C")
+    print(f"Humidity: {sensor.relative_humidity:.1f} %")
+    time.sleep(5)
+
